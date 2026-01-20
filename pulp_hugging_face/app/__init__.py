86b2fa4ccec403b7c2dc70052630e4d60ecad53d
pulpbot <pulp-infra@redhat.com>
2026-01-20 09:58:04 +0000

    Bump version: 0.2.2 → 0.2.3.dev

--- pulp_hugging_face/app/__init__.py
+++ pulp_hugging_face/app/__init__.py
@@ -6,6 +6,6 @@ class PulpHuggingFacePluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_hugging_face.app"
     label = "hugging_face"
-    version = "0.2.2"
+    version = "0.2.3.dev"
     python_package_name = "pulp_hugging_face"
     domain_compatible = True
